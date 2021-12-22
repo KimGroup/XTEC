38d2509ec86b74dc2d6e866727922f6da6459059
Ray Osborn <rayosborn@mac.com>
2021-12-22 08:13:45 -0600

    Add extra metadata to setup

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ from distutils.core import setup
 
 setup(
     name="X-TEC",
-    version="1.0.0",
+    version="0.1.0",
     description="X-ray Temperature Clustering",
+    author="Jordan Venderley",
+    maintainer="Krishnanand Mallayya",
+    maintainer_email="krishnanandmallayya@gmail.com",
     url="https://cels.anl.gov/axmas/",
+    download_url="https://github.com/KimGroup/X-TEC",
     packages=["xtec"],
     python_requires=">=3.7",
     install_requires=["numpy", "matplotlib", "scikit-learn", "scipy"],
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
