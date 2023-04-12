e4a602b3adac404470cecbc4dee5751535eea4e4
Ray Osborn <rayosborn@mac.com>
2023-04-12 16:58:21 -0500

    Add an entry point to make the plugin discoverable

--- src/xtec/plugins/xtec/__init__.py
+++ src/xtec/plugins/xtec/__init__.py
@@ -2,9 +2,9 @@ from . import GMM_clustering, BIC_score
 
 
 def plugin_menu():
-    menu = 'XTEC'
+    menu = "XTEC"
     actions = []
-    actions.append(('XTEC', GMM_clustering.show_dialog))
-    actions.append(('BIC score', BIC_score.show_dialog))
+    actions.append(("XTEC", GMM_clustering.show_dialog))
+    actions.append(("BIC score", BIC_score.show_dialog))
 
     return menu, actions
